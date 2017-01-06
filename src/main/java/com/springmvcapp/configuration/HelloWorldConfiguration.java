a37ed45ec2387eedece0e86af269f9cbc0e1613e
강성길 <KangSeongGil@gangseonggilui-MacBook-Pro.local>
2017-01-06 10:28:44 +0900

    Hello World

---
+++ src/main/java/com/springmvcapp/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,29 @@
+package com.springmvcapp.configuration;
+
+/**
+ * Created by KangSeongGil on 2017. 1. 6..
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.springmvcapp")
+public class HelloWorldConfiguration {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+
+}
---
+++ src/main/java/com/springmvcapp/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+
+package com.springmvcapp.controller;
+/**
+ * Created by KangSeongGil on 2017. 1. 6..
+ */
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "welcome";
+    }
+
+}
