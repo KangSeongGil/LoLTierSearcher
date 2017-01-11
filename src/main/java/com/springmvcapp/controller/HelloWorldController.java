9cc6a6c3b55cb7275c503743472e6f492d1d4800
강성길 <KangSeongGil@gangseonggilui-MacBook-Pro.local>
2017-01-11 16:59:27 +0900

    VelocityTest3

--- src/main/java/com/springmvcapp/controller/HelloWorldController.java
+++ src/main/java/com/springmvcapp/controller/HelloWorldController.java
@@ -3,19 +3,22 @@ package com.springmvcapp.controller;
 /**
  * Created by KangSeongGil on 2017. 1. 6..
  */
-
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
 
+
+
+@Controller
+@Service
 public class HelloWorldController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String sayHello(ModelMap model) {
+    @RequestMapping(value ={"/","welcome"}, method = RequestMethod.GET)
+    public String sayHello(Model model) {
         model.addAttribute("greeting", "안녕하세요 롤 티어 검색기 입니다");
         return "welcome";
     }
