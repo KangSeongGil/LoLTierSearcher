9844eca0bf85714a83d4634fcc7f290c322675fa
강성길 <KangSeongGil@gangseonggilui-MacBook-Pro.local>
2017-01-11 16:02:37 +0900

    VelocityTest1

--- src/main/java/com/springmvcapp/controller/HelloWorldController.java
+++ src/main/java/com/springmvcapp/controller/HelloWorldController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
+
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String sayHello(ModelMap model) {
         model.addAttribute("greeting", "안녕하세요 롤 티어 검색기 입니다");
         return "welcome";
