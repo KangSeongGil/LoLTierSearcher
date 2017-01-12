2fa131c53d80e328da825ca31b8ad15a9d94c246
KangSeongGil <k.seonggil@gmail.com>
2017-01-13 03:14:28 +0900

    tiles test14

--- src/main/java/com/lolTierSearch/springmvcapp/controller/HelloWorldController.java
+++ src/main/java/com/lolTierSearch/springmvcapp/controller/HelloWorldController.java
@@ -4,19 +4,11 @@ package com.lolTierSearch.springmvcapp.controller;
  * Created by KangSeongGil on 2017. 1. 6..
  */
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 @Controller
@@ -27,12 +19,6 @@ public class HelloWorldController {
     public String sayHello(Model model) {
         model.addAttribute("greeting", "안녕하세요 롤 티어 검색기 입니다");
         return "welcome";
-    }
-
-    public ModelAndView list(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        Model model = new ExtendedModelMap();
-        model.addAttribute("greeting", "안녕하세요 롤 티어 검색기 입니다");
     }
 
 }
