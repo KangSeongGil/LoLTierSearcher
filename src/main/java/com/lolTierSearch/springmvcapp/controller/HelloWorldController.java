0c2cb4db043b5edb79f81f5c58b5d2fb41a2c04f
KangSeongGil <k.seonggil@gmail.com>
2017-01-23 20:33:04 +0900

    Velocity Iss4

--- src/main/java/com/lolTierSearch/springmvcapp/controller/HelloWorldController.java
+++ src/main/java/com/lolTierSearch/springmvcapp/controller/HelloWorldController.java
@@ -4,24 +4,22 @@ package com.lolTierSearch.springmvcapp.controller;
  * Created by KangSeongGil on 2017. 1. 6..
  */
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-@RequestMapping(value = "/")
+@RequestMapping("/")
 public class HelloWorldController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public ModelAndView viewHello(Model model) {
-        String message = "안녕하세요 롤 검색기 입니다";
-        return new ModelAndView("hello", "greeting", message);
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(Model model) {
+        model.addAttribute("greeting", "안녕하세요 롤 티어 검색기 입니다");
+        return "welcome";
     }
 
 }
-
-
-
