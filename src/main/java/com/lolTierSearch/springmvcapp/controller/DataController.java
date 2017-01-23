91f46212d9f414ce6689f23f8339fd8b34e98056
KangSeongGil <k.seonggil@gmail.com>
2017-01-23 21:09:13 +0900

    Velocity Iss4

--- src/main/java/com/lolTierSearch/springmvcapp/controller/DataController.java
+++ src/main/java/com/lolTierSearch/springmvcapp/controller/DataController.java
@@ -4,9 +4,7 @@ package com.lolTierSearch.springmvcapp.controller;
  * Created by KangSeongGil on 2017. 1. 6..
  */
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,12 +12,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
-public class HelloWorldController {
+public class DataController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String sayHello(Model model) {
         model.addAttribute("greeting", "안녕하세요 롤 티어 검색기 입니다");
-        return "welcome";
+        return "/WEB-INF/views/index.vm";
     }
 
 }
