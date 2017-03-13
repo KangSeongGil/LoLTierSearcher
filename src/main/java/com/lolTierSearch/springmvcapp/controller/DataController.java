e36ccd391cbd292a29c6489fb85d461a301193cd
KangSeongGil <k.seonggil@gmail.com>
2017-03-13 14:19:20 +0900

    velocity tiles intergration

--- src/main/java/com/lolTierSearch/springmvcapp/controller/DataController.java
+++ src/main/java/com/lolTierSearch/springmvcapp/controller/DataController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
+
 public class DataController {
 
     @RequestMapping(method = RequestMethod.GET)
