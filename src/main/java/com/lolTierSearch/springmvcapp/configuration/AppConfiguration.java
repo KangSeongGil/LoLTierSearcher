c73b1bab284e431fe65f4b3747a848b5928b0e33
KangSeongGil <k.seonggil@gmail.com>
2017-03-21 02:24:08 +0900

    change java config

--- src/main/java/com/lolTierSearch/springmvcapp/configuration/AppConfiguration.java
+++ src/main/java/com/lolTierSearch/springmvcapp/configuration/AppConfiguration.java
@@ -3,6 +3,7 @@ package com.lolTierSearch.springmvcapp.configuration;
 /**
  * Created by KangSeongGil on 2017. 3. 20..
  */
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -15,6 +16,8 @@ import org.springframework.web.servlet.view.velocity.VelocityConfig;
 import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
 import org.springframework.web.servlet.view.velocity.VelocityToolboxView;
 import org.springframework.web.servlet.view.velocity.VelocityViewResolver;
+
+import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = "com.lolTierSearch.springmvcapp")
@@ -33,6 +36,11 @@ public class AppConfiguration implements ApplicationContextAware {
         cfg.setResourceLoaderPath("/WEB-INF/views/");
         cfg.setConfigLocation(context
                 .getResource("/WEB-INF/resource/velocity/velocity.properties"));
+
+        Properties properties = new Properties();
+        properties.setProperty("input.encoding", "UTF-8");
+        properties.setProperty("output.encoding", "UTF-8");
+        cfg.setVelocityProperties(properties);
         return cfg;
     }
 
