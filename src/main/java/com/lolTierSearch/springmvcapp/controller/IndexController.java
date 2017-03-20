8f4906cc91cb4a62b3d191bbcdba2636f1178022
KangSeongGil <k.seonggil@gmail.com>
2017-03-21 01:30:42 +0900

    change java config

--- src/main/java/com/lolTierSearch/springmvcapp/controller/IndexController.java
+++ src/main/java/com/lolTierSearch/springmvcapp/controller/IndexController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping("/main")
     public String indexHandler(ModelMap model) {
         return "/WEB-INF/views/index_velocity";
     }
