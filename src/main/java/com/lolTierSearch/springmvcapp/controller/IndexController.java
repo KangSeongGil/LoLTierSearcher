a7dcd4e37d2b87a0088157b56b1588cec0082ca9
KangSeongGil <k.seonggil@gmail.com>
2017-04-20 01:29:12 +0900

    finish

--- src/main/java/com/lolTierSearch/springmvcapp/controller/IndexController.java
+++ src/main/java/com/lolTierSearch/springmvcapp/controller/IndexController.java
@@ -1,19 +1,24 @@
 package com.lolTierSearch.springmvcapp.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
 /**
- * Created by KangSeongGil on 2017. 3. 15..
+ * Created by KangSeongGil on 2017. 1. 6..
  */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
-    @RequestMapping("/")
-    public ModelAndView indexHandler(ModelMap model) {
-        String message = "Hello LoL Tier Seacher";
-        return new ModelAndView("index_velocity", "greeting" , message);
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(Model model) {
+        model.addAttribute("greeting", "안녕하세요 롤 티어 검색기 입니다");
+        return "welcome";
     }
+
 }
