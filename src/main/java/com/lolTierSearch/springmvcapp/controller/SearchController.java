a7eb71f1c45d70bb3558418a90b827caef3aa4af
KangSeongGil <k.seonggil@gmail.com>
2017-04-25 00:51:39 +0900

    get_test

--- src/main/java/com/lolTierSearch/springmvcapp/controller/SearchController.java
+++ src/main/java/com/lolTierSearch/springmvcapp/controller/SearchController.java
@@ -9,14 +9,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/intergratedSearch")
 public class SearchController{
     @RequestMapping(method = RequestMethod.GET)
-    public String initSearch(@RequestParam("ID") String ID, Model model) {
+    public String initSearch(HttpServletRequest request, Model model) {
+        String ID = request.getParameter("searchID");
         RestTemplate restTemplate = new RestTemplate();
         String userInfo = restTemplate.getForObject("https://kr.api.riotgames.com" +
                         "/lol/summoner/v3/summoners/by-name/" + ID +
